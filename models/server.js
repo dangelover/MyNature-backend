efedab50ec1d9651ea50045b48d42aeb284c64f2
Kevin Castro Campos <45100343+dangelover@users.noreply.github.com>
2022-12-12 20:39:17 -0500

    Default port update

--- models/server.js
+++ models/server.js
@@ -5,7 +5,7 @@ class Server {
   constructor() {
     //creamos una instancia de express, cuando se lance se creara como una propiedad del constructor
     this.app = express();
-    this.port = process.env.PORT;
+    this.port = process.env.PORT || 3000;
     this.usuariosPath = "/api/usuarios";
     //Middlewares
     this.middleware();
